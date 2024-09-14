a39c8334d424925bd52fc6f37def4f36727c285a
danchoys <azarov.daniil@inbox.ru>
2024-09-14 15:20:01 +0300

    add tests

--- gtest_ip_filter.cpp
+++ gtest_ip_filter.cpp
@@ -1,7 +1,92 @@
-// #include "ip_filter.cpp"
-// #include <gtest/gtest.h>
+#include "ip_filter.h"
+#include "gtest_data.h"
 
-// TEST(gtest_version, test)
-// {
-//     ASSERT_TRUE(version() > 0);
-// }
+#include <gtest/gtest.h>
+
+// filter by first byte //
+TEST(gtest_ip_filter, test_fist_byte_filter)
+{
+    static const std::vector<uint32_t> result
+    {
+        parseIp("1.231.69.33"),
+        parseIp("1.87.203.225"),
+        parseIp("1.70.44.170"),
+        parseIp("1.29.168.152"),
+        parseIp("1.1.234.8"),
+    };
+
+    std::stringstream logsStream(GTestData::IpLog);
+    std::vector<uint32_t> filterIps = filter(parseLogs(logsStream), 1);
+
+    ASSERT_EQ(filterIps.size(), result.size()) << "Vectors filterIps and result are of unequal length " << std::endl;
+    for (size_t i = 0; i < filterIps.size(); ++i)
+        EXPECT_EQ(filterIps[i], result[i]) << "Vectors filterIps and result differ at index " << i;
+}
+
+// filter by first and second bytes //
+TEST(gtest_ip_filter, test_fist_second_byte_filter)
+{
+    static const std::vector<uint32_t> result
+    {
+        parseIp("46.70.225.39"),
+        parseIp("46.70.147.26"),
+        parseIp("46.70.113.73"),
+        parseIp("46.70.29.76"),
+    };
+
+    std::stringstream logsStream(GTestData::IpLog);
+    std::vector<uint32_t> filterIps = filter(parseLogs(logsStream), 46, 70);
+
+    ASSERT_EQ(filterIps.size(), result.size()) << "Vectors filterIps and result are of unequal length " << std::endl;
+    for (size_t i = 0; i < filterIps.size(); ++i)
+        EXPECT_EQ(filterIps[i], result[i]) << "Vectors filterIps and result differ at index " << i;
+}
+
+// filter by any byte //
+TEST(gtest_ip_filter, test_any_byte_filter)
+{
+    static const std::vector<uint32_t> result
+    {
+        parseIp("186.204.34.46"),
+        parseIp("186.46.222.194"),
+        parseIp("185.46.87.231"),
+        parseIp("185.46.86.132"),
+        parseIp("185.46.86.131"),
+        parseIp("185.46.86.131"),
+        parseIp("185.46.86.22"),
+        parseIp("185.46.85.204"),
+        parseIp("185.46.85.78"),
+        parseIp("68.46.218.208"),
+        parseIp("46.251.197.23"),
+        parseIp("46.223.254.56"),
+        parseIp("46.223.254.56"),
+        parseIp("46.182.19.219"),
+        parseIp("46.161.63.66"),
+        parseIp("46.161.61.51"),
+        parseIp("46.161.60.92"),
+        parseIp("46.161.60.35"),
+        parseIp("46.161.58.202"),
+        parseIp("46.161.56.241"),
+        parseIp("46.161.56.203"),
+        parseIp("46.161.56.174"),
+        parseIp("46.161.56.106"),
+        parseIp("46.161.56.106"),
+        parseIp("46.101.163.119"),
+        parseIp("46.101.127.145"),
+        parseIp("46.70.225.39"),
+        parseIp("46.70.147.26"),
+        parseIp("46.70.113.73"),
+        parseIp("46.70.29.76"),
+        parseIp("46.55.46.98"),
+        parseIp("46.49.43.85"),
+        parseIp("39.46.86.85"),
+        parseIp("5.189.203.46")
+    };
+
+    std::stringstream logsStream(GTestData::IpLog);
+    std::vector<uint32_t> filterIps = filter_any(parseLogs(logsStream), 46);
+
+    ASSERT_EQ(filterIps.size(), result.size()) << "Vectors filterIps and result are of unequal length " << std::endl;
+    for (size_t i = 0; i < filterIps.size(); ++i)
+        EXPECT_EQ(filterIps[i], result[i]) << "Vectors filterIps and result differ at index " << i;
+}
--- ip_filter.cpp
+++ ip_filter.cpp
@@ -1,31 +1,8 @@
-#include <cassert>
-#include <cstdlib>
+#include "ip_filter.h"
+
 #include <iostream>
-#include <string>
-#include <set>
-#include <vector>
-#include <string_view>
 #include <functional>
 #include <sstream>
-#include <utility>
-
-std::vector<std::string_view> split(std::string_view str, char d);
-uint32_t parseIp(std::string_view ipAddress);
-
-std::multiset<uint32_t, std::greater<uint32_t>> parseLogs(std::istream& stream)
-{
-    std::multiset<uint32_t, std::greater<uint32_t>> ipPool;
-    std::string ipStr = "";
-    while(std::getline(stream, ipStr))
-    {
-        if (ipStr.empty()) 
-            break; 
-        
-        ipPool.emplace(parseIp(split(ipStr,' ').at(0)));
-    }
-
-    return ipPool;
-}
 
 std::vector<std::string_view> split(std::string_view str, char d)
 {
@@ -44,6 +21,21 @@ std::vector<std::string_view> split(std::string_view str, char d)
     r.push_back(str.substr(start));
 
     return r;
+}
+
+std::multiset<uint32_t, std::greater<uint32_t>> parseLogs(std::istream& stream)
+{
+    std::multiset<uint32_t, std::greater<uint32_t>> ipPool;
+    std::string ipStr = "";
+    while(std::getline(stream, ipStr))
+    {
+        if (ipStr.empty()) 
+            break; 
+        
+        ipPool.emplace(parseIp(split(ipStr,' ').at(0)));
+    }
+
+    return ipPool;
 }
 
 uint32_t parseIp(std::string_view ipAddress) 
@@ -109,91 +101,4 @@ std::vector<uint32_t> filter_any(const std::multiset<uint32_t, std::greater<uint
     }
 
     return filterNums;
-}
-
-int main()
-{
-    try
-    {
-        //reverse lexicographically sort multiset
-        std::multiset<uint32_t, std::greater<uint32_t>> ipPool = parseLogs(std::cin);
-
-        // print reverse lexicographically sort multiset //
-        for(auto ip : ipPool)
-        {
-            printIp(ip);
-        }
-        // RESULT:
-        // 222.173.235.246
-        // 222.130.177.64
-        // 222.82.198.61
-        // ...
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // filter by first byte and output //
-        std::vector<uint32_t> filterIps = filter(ipPool, 1);
-        printIpVec(filterIps);
-        // RESULT:
-        // 1.231.69.33
-        // 1.87.203.225
-        // 1.70.44.170
-        // 1.29.168.152
-        // 1.1.234.8
-
-        // filter by first and second bytes and output //
-        filterIps = filter(ipPool, 46, 70);
-        printIpVec(filterIps);
-        // RESULT:
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-
-        // filter by any byte and output //
-        filterIps = filter_any(ipPool, 46);
-        printIpVec(filterIps);
-        // RESULT:
-        // 186.204.34.46
-        // 186.46.222.194
-        // 185.46.87.231
-        // 185.46.86.132
-        // 185.46.86.131
-        // 185.46.86.131
-        // 185.46.86.22
-        // 185.46.85.204
-        // 185.46.85.78
-        // 68.46.218.208
-        // 46.251.197.23
-        // 46.223.254.56
-        // 46.223.254.56
-        // 46.182.19.219
-        // 46.161.63.66
-        // 46.161.61.51
-        // 46.161.60.92
-        // 46.161.60.35
-        // 46.161.58.202
-        // 46.161.56.241
-        // 46.161.56.203
-        // 46.161.56.174
-        // 46.161.56.106
-        // 46.161.56.106
-        // 46.101.163.119
-        // 46.101.127.145
-        // 46.70.225.39
-        // 46.70.147.26
-        // 46.70.113.73
-        // 46.70.29.76
-        // 46.55.46.98
-        // 46.49.43.85
-        // 39.46.86.85
-        // 5.189.203.46
-    }
-    catch(const std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-
-    return 0;
 }
